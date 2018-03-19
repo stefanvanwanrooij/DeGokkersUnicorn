53bbe01b1b97c66cfbbe85a6c12b2cdf05da8bb5
timovanbijnen <timovanbijnen@hotmail.nl>
2018-03-19 14:34:10 +0100

    loginsystem
    database edit

--- gokker_website/includes/databaseConnecter.php
+++ gokker_website/includes/databaseConnecter.php
@@ -1,8 +1,8 @@
-<?php //DataBaseConnector.php
-require ('../config/config.php');
+<?php /* //DataBaseConnector.php
+require ('../includes/config.php');
 try{
     $conn = new PDO("mysql:dbname=$dbName;host=$dbServername",$dbUsername,$dbPassword);
-
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 }catch (PDOException $exception){
     die('er is een fout opgetreden bij het verbinden met de database.php, hier heb je hem'. $exception->getMessage());
